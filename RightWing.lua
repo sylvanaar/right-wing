004c643cf219c555f35f6e0c0715f5efda15ee48
sylvanaar <sylvanaar@localhost>
2011-04-26 08:29:19 -0400

    um, there were no files in this addon LOL. TOC 40100

---
+++ RightWing.lua
@@ -0,0 +1,6 @@
+MainMenuBarArtFrame:SetAttribute("unit2", "player"); 
+MultiBarLeft:SetAttribute("unit2", "player");
+MultiBarRight:SetAttribute("unit2", "player");
+MultiBarBottomLeft:SetAttribute("unit2", "player");
+MultiBarBottomRight:SetAttribute("unit2", "player");
+BonusActionBarFrame:SetAttribute("unit2", "player");
